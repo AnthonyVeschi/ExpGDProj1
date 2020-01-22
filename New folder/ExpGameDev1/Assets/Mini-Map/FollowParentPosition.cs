a1cefd8b0fdff090daaa0dfbf8096d5c1ee62b04
korstrack <34286305+korstrack@users.noreply.github.com>
2020-01-22 16:58:09 -0500

    Undated Mini Map
    slight change to how it works and an icon for the player

---
+++ New folder/ExpGameDev1/Assets/Mini-Map/FollowParentPosition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowParentPosition : MonoBehaviour
+{
+    public Transform target;
+
+    void LateUpdate(){
+
+        Vector3 newPosition = target.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+    }
+}
