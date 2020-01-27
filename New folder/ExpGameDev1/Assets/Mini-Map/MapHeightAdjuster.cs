3b6bba14114d615fcc2664153483bbf146fdcb18
korstrack <korstrack.tereans@gmail.com>
2020-01-27 13:22:10 -0500

    MiniMap and phone
    I have the height fixes for the minimap in now - should change based on your height. I didn't get a chance to test it a lot yet. I also threw in my mock up of the phone icon as and will be working on the full version next.

---
+++ New folder/ExpGameDev1/Assets/Mini-Map/MapHeightAdjuster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapHeightAdjuster : MonoBehaviour
+{
+	public Transform player;
+	public Camera cam;
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.position.y < 10){
+        	cam.cullingMask = 1 << LayerMask.NameToLayer("MiniMapFloor0");
+        }
+        else if((player.position.y > 10) && (player.position.y < 20)){
+        	cam.cullingMask = 1 << LayerMask.NameToLayer("MiniMapFloor1");
+        }
+        else if(player.position.y > 20){
+        	cam.cullingMask = 1 << LayerMask.NameToLayer("MiniMapFloor2");
+        }
+    }
+}
