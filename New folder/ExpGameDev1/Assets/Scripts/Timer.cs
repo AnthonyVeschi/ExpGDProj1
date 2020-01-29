2f2787a5c5499c229ddac293c21561b91b1fda3d
Anthony Veschi <anthonyveschi@gmail.com>
2020-01-29 04:45:13 -0500

    simple timer / game over screen added

---
+++ New folder/ExpGameDev1/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeStart;
+    Text t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeStart = 120f;
+
+        t = gameObject.GetComponent<Text>();
+        t.text = timeStart.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeStart -= Time.deltaTime;
+        t.text = Mathf.Round(timeStart).ToString();
+
+        if (timeStart <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
